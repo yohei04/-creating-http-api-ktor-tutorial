85b46d39e6a787ad2331763fde6bddf12b8ddbd1
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-01-12 11:17:52 +0900

    finish ch3 Customer routes

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,23 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import customerRouting
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomerRoutes()
+}
 
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
+    }
 }
